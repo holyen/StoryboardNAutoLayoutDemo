8fafe59ebecd380ca3d518bd52210052bbb41b18
Holyen Zou <holyenzou@gmail.com>
2014-09-22 13:41:51 +0800

    inital code

---
+++ StoryboardNAutoLayoutDemo/StoryboardNAutoLayoutDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoryboardNAutoLayoutDemo
+//
+//  Created by iOS on 14-9-22.
+//  Copyright (c) 2014年 0jiedian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StoryboardNAutoLayoutDemo/StoryboardNAutoLayoutDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoryboardNAutoLayoutDemo
+//
+//  Created by iOS on 14-9-22.
+//  Copyright (c) 2014年 0jiedian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
